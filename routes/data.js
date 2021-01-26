05b78a64e0116a71802538e131074e538a4b0b21
Augusto Cezar <augusto.c.ferreira@hotmail.com>
2021-01-25 21:37:45 -0300

    correções do deploy - deixando horario correto

--- routes/data.js
+++ routes/data.js
@@ -5,9 +5,9 @@ var Data = require('../model/db');
 /* GET home page. */
 router.post('/', async function(req, res, next) {
 
-  var today = new Date().toUTCString();
-  var date = today.getDate()+'-'+(today.getMonth()+1).toLocaleString()+'-'+today.getFullYear();
-  var time = today.getHours().toLocaleString() + ":" + today.getMinutes().toLocaleString() + ":" + today.getSeconds().toLocaleString();  
+  var today = new Date();
+  var date = today.getDate().toUTCString()+'-'+(today.getMonth()+1).toUTCString()+'-'+today.getFullYear();
+  var time = today.getHours().toLocaleString()-3 + ":" + today.getMinutes().toLocaleString() + ":" + today.getSeconds().toLocaleString();  
   
   
   let posts = await Data.insert(req.query.longitude, req.query.latitude, req.query.velocity,date, time);
