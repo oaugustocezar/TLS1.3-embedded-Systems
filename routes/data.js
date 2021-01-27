0a256e1bffd95121b8d7b3f23c03d79156af7bd9
Augusto Cezar <augusto.c.ferreira@hotmail.com>
2021-01-27 17:35:46 -0300

    ajustando as requisições

--- routes/data.js
+++ routes/data.js
@@ -8,9 +8,11 @@ router.post('/', async function(req, res, next) {
   var today = new Date();
   var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
   var time = (today.getHours()) + ":" + today.getMinutes() + ":" + today.getSeconds();  
+
+  console.log(res.req);
   
   
-  let posts = await Data.insert(req.query.longitude, req.query.latitude, req.query.velocity,date, time);
+  let posts = await Data.insert(res.req.body.longitude, res.req.body.latitude, res.req.body.velocity,date, time);
   res.status(200).json({"mensagem": "Mensagem postada com sucesso"});
 
 
